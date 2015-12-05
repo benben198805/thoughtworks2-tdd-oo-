efd037d0d612c4bbae0c2a326e96549cc252c52c
ben <benben198805@126.com>
2015-12-05 09:54:49 +0800

    update profession

---
+++ src/test/java/oo/ProfessionTest.java
@@ -0,0 +1,21 @@
+package java.oo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ben on 15-12-4.
+ */
+public class ProfessionTest {
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGenWeaponEffect() throws Exception {
+
+    }
+}
